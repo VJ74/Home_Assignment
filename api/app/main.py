5cc90184c1a9122f7fa36c913dba7371cf2fae97
VJ74 <vekeman.j@skynet.be>
2022-02-05 14:41:41 +0100

    updates requirements

--- api/app/main.py
+++ api/app/main.py
@@ -7,18 +7,16 @@ from fastapi.responses import Response, FileResponse
 from typing import List
 import tensorflow as tf
 import numpy as np
-
-
-
+import librosa 
 
 from enum import Enum
 
 class FileType(Enum):
     audio: str = 'AUDIO'
     
-#BASE_PATH = '/mnt/data/storage'
-#AUDIO_PATH = os.path.join(BASE_PATH, FileType.audio.value)
-#os.makedirs(AUDIO_PATH, exist_ok=True)
+BASE_PATH = '/mnt/data/storage'
+AUDIO_PATH = os.path.join(BASE_PATH, FileType.audio.value)
+os.makedirs(AUDIO_PATH, exist_ok=True)
   
 app = FastAPI()
    
@@ -28,8 +26,8 @@ loaded_model=tf.keras.models.load_model('my_model.h5')
   
 # Predict if a defect occurred or not, based on an sound file of 1s in length
    
-#def predict_audio_file(file):
-    #audio, sr = librosa.load(file, 22000)
+def predict_audio_file(file):
+    audio, sr = librosa.load(file, 22000)
 
 
 def predict_audio_file(file_path):
@@ -45,29 +43,29 @@ def predict_audio_file(file_path):
  
 @app.get("/")
 def root():
-    return {"message":"Hello World"}
+    return {"message":"Hello World - This sounds good"}
   
-#@app.get("/audio", response_model=List[str])
-#def getAllAudio():
-    #return os.listdir(AUDIO_PATH)
+@app.get("/audio", response_model=List[str])
+def getAllAudio():
+    return os.listdir(AUDIO_PATH)
   
-#@app.get("/audio/{filename}", response_class=FileResponse)
-#def getAudioById(filename: str):
-   # try:
-       # file = os.path.join(AUDIO_PATH, filename)
-       # return predict_audio_file(file)
-    #except Exception as e:
-       # return HTTPException(500, f'Something went wrong while trying to return file {filename}')
+@app.get("/audio/{filename}", response_class=FileResponse)
+def getAudioById(filename: str):
+    try:
+        file = os.path.join(AUDIO_PATH, filename)
+        return predict_audio_file(file)
+    except Exception as e:
+        return HTTPException(500, f'Something went wrong while trying to return file {filename}')
   
-#@app.post("/upload")
-#def uploadFile(file: UploadFile = File(...), type: FileType = FileType.default):
-  #  print(f"Upload type: {type}")
-   # print(f"Upload type: {type.value}")
-   # fileLocation = os.path.join(BASE_PATH, type.value, file.filename)
-   # try:
-       # open(fileLocation, 'wb').write(file.file.read())
-    #except Exception as e:
-        #print(e)
-        #return HTTPException(500, f'Something went wrong while trying to upload file {file.filename}')
+@app.post("/upload")
+def uploadFile(file: UploadFile = File(...), type: FileType = FileType.default):
+    print(f"Upload type: {type}")
+    print(f"Upload type: {type.value}")
+    fileLocation = os.path.join(BASE_PATH, type.value, file.filename)
+    try:
+        open(fileLocation, 'wb').write(file.file.read())
+    except Exception as e:
+        print(e)
+        return HTTPException(500, f'Something went wrong while trying to upload file {file.filename}')
    
-    #return Response(f'Uploaded file to {fileLocation}', 200)
+    return Response(f'Uploaded file to {fileLocation}', 200)
